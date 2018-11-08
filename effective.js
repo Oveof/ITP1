58f8b1ecf700153be2b97ae573672d3f7ec2ad98
oveof <ovedy@oftedal.com>
2018-11-08 19:24:11 +0100

    Begun working on images/svgs

--- effective.js
+++ effective.js
@@ -1,14 +1,14 @@
-"stone": {
+var stone = {
   "name": "stein",
   "effective": "scissor",
   "weak": "paper"
 }
-"paper": {
+var paper = {
   "name": "papir",
   "effective": "stone",
   "weak": "scissor"
 }
-"scissor": {
+var scissors = {
   "name": "Saks",
   "effective": "paper",
   "weak": "stone"
--- vsCpu.js
+++ vsCpu.js
@@ -1,14 +1,16 @@
-var p1, p2;
-function check(p1, p2) {
-  return p1 ? window[p2].weak : "p1"
-         p2 ? window[p1].weak : "p2"
-}
+//CPU
 function cpu() {
-  var rand = Math.floor(Math.random() * 2)
-  return rand ? 0 : "stone"
-         rand ? 1 : "scissor"
-         rand ? 2 : "paper"
+  var rand = Math.floor(Math.random() * (4 - 1)) + 1;
+  return rand == 1 ? "stone"
+       : rand == 2 ? "scissors"
+       : "paper"
 }
+window.addEventListener("keydown", onKeyDown, false);
+window.addEventListener("keyup", onKeyUp, false);
+//Player controls
+var keyA = false;
+var keyS = false;
+var keyD = false;
 function onKeyDown(event) {
   var keyCode = event.keyCode;
   switch (keyCode) {
@@ -24,18 +26,31 @@ function onKeyDown(event) {
     default:
   }
 }
-function onKeyUp() {
+function onKeyUp(event) {
   var keyCode = event.keyCode;
+  selected1 = "";
+  img1 = document.getElementById('imgp1')
   switch (keyCode) {
     case 65: //a
       keyA = false;
+      selected1 = "stone";
+      
       break;
     case 83: //s
       keyS = false;
+      selected1 = "scissors";
       break;
     case 68: //d
       keyD = false;
+      selected1 = "paper";
       break;
     default:
   }
+}
+
+//Calculate who wins
+var p1, p2;
+function check(p1, p2) {
+  return p1 ? Window[p2].weak : "p1"
+  p2 ? Window[p1].weak : "p2"
 }
